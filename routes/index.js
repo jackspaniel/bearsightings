db5a903a17224188f29fe7c17de3d5f569afb945
Matt Savino <m.savino@doculife.com>
2018-08-02 19:20:41 -0700

    Fixed package.json, added count display

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+let count = 0; // never ever do this in the real world
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Matt has created a node app on AWS OMGOMGOMGOMG' });
+  count++;
+  res.render('index', { 
+    title: 'Matt has created a node app on AWS OMGOMGOMGOMG',
+    count });
 });
 
 module.exports = router;
